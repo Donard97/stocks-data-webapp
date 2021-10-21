bf64d7d26ab35c7e875aed723130329026867d9e
Donard97 <donardgolaj@gmail.com>
2021-10-21 15:20:32 +0200

    Dispatch data

---
+++ src/pages/Main.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { listAPIcall } from '../redux/home/home';
+import Top from '../components/Top';
+import Home from '../components/Home';
+
+const Main = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(listAPIcall());
+  }, []);
+  const result = useSelector((state) => state.countriesReducer);
+  return (
+    <>
+      <Top />
+      <div>
+        <Home nasdaq={result} />
+      </div>
+    </>
+  );
+};
+
+export default Main;
