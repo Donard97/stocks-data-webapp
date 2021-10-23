9bbfa1387a16eb45d8188cfd19f38cdb34af693d
Donard97 <donardgolaj@gmail.com>
2021-10-23 11:59:22 +0200

    Add filter to search companies

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Each from './Each';
 
 const Home = ({ nasdaq }) => {
   const companies = nasdaq;
-  companies.sort((a, b) => b.changesPercentage - a.changesPercentage);
+  const [fetchedData, setFetchedData] = useState(companies);
+  const [searchValue, setSearchValue] = useState('');
+  fetchedData.sort((a, b) => b.changesPercentage - a.changesPercentage);
 
+  const handleSearch = (e) => {
+    // console.log(e.target.value);
+    setSearchValue(e.target.value);
+  };
+
+  useEffect(() => {
+    setFetchedData(() => companies);
+  }, [companies]);
+
+  const filteredData = [];
+
+  // console.log('fetchedData', fetchedData);
+  if (fetchedData.length > 0 && searchValue) {
+    fetchedData.filter((each) => {
+      if (each.companyName.includes(searchValue)) {
+        filteredData.push(each);
+      }
+      return filteredData;
+    });
+  }
+  console.log('filteredData', filteredData);
+
+  console.log(searchValue);
   return (
     <div>
       <div className="full-width">
         <div className="search-container">
-          <p>Search By Name</p>
-          <input id="country-search" className="search-field" placeholder="Search" type="text" />
+          <p>Search by company name</p>
+          <input id="country-search" className="search-field" placeholder="Search" type="text" onChange={handleSearch} />
         </div>
         <h5 className="list-heading">Top Performers</h5>
       </div>
       <div className="container">
-        {companies.map((item, index) => (
+        {filteredData && filteredData.map((item, index) => (
           <div key={item.ticker} className="container-child">
             <Each
               name={item.ticker}
@@ -26,10 +51,32 @@ const Home = ({ nasdaq }) => {
             />
           </div>
         ))}
+        {filteredData.length === 0 && fetchedData.map((item, index) => (
+          <div key={item.ticker} className="container-child">
+            <Each
+              name={item.ticker}
+              price={item.price}
+              index={index}
+              company={item.companyName}
+            />
+          </div>
+        ))}
+
       </div>
     </div>
   );
 };
+
+// {fetchedData.map((item, index) => (
+//   <div key={item.ticker} className="container-child">
+//     <Each
+//       name={item.ticker}
+//       price={item.price}
+//       index={index}
+//       company={item.companyName}
+//     />
+//   </div>
+// ))}
 
 Home.propTypes = {
   nasdaq: PropTypes.instanceOf(Object).isRequired,
